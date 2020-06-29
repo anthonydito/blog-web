5ecb443432d6eccd3bb09e1cace58081d10fd81c
Anthony Dito <anthonymdito@gmail.com>
2020-06-29 15:22:59 -0700

    Created the blog post component

---
+++ src/BlogPost.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BlogPost() {
+    return (
+        <div>
+            <p>The blog post text will go here</p>
+            <p>here is where we will put the date</p>
+        </div>
+    );
+}
+
+export default BlogPost;

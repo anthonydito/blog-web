9cbadfe5b661449a47e46253a13a6850dbfbd976
Anthony Dito <anthonymdito@gmail.com>
2021-01-31 17:17:02 -0800

    Update the App.js for our work on the create account screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Blogs from './Blogs';
+import CreateAccount from './CreateAccount';
 
 function App() {
 
     return (
-        <Blogs />
+        <CreateAccount />
     );
 }
 
---
+++ src/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateAccount() {
+    return (
+        <div>
+            <h1>Create Account</h1>
+            <p>Create account using the form below</p>
+        </div>
+    );
+}
+
+export default CreateAccount;

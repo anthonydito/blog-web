763cabb1233fab6e26686b5d312392206dcc68cb
Anthony Dito <anthonymdito@gmail.com>
2021-11-14 22:39:31 -0800

    route based on whether or not there is an access token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import CreateAccount from './CreateAccount';
+import Blogs from './Blogs';
 
 function App() {
 
-    return (
-        <CreateAccount />
-    );
+    const hasAccessToken = localStorage.getItem("accessToken") !== null;
+
+    if (hasAccessToken) {
+        return (
+            <Blogs />
+        );
+    } else {
+        return (
+            <CreateAccount />
+        );
+    }
 }
 
 export default App
